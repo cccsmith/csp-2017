bcd7f5138a16d81e090fdac81c86516b3b21ab36
cccsmith <30420942+cccsmith@users.noreply.github.com>
2017-07-31 11:58:50 -0700

    Add files via upload

---
+++ Programs/unitconv.py
@@ -0,0 +1,22 @@
+# units converter 1.0
+
+print "Miles to Kilometers"
+print "How many miles?"
+miles = raw_input()
+km=float(miles)*1.60934
+print km,"kilometers"
+
+
+print "Kilometere to Miles"
+print "How many Kilometers?"
+kilometers = raw_input()
+miles=float(kilometers)*0.621371
+print miles,"miles"
+
+
+print "What degrees is it fahrenheit to celsius"
+print "How many degrees in fahrenheit?"
+f = raw_input()
+c=((float(f)-32)/(9/5))
+print c,"celsius"
+
